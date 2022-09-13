44dfe8cd39869011139f4f041dbb9f893c9e3ae5
Alexandra <sobakasasha114@gmail.com>
2022-09-13 23:01:31 +0300

    init commit

---
+++ src/main/java/com/volushkova/apifirst/apifirstmockclient/ApiFirstMockClientApplication.java
@@ -0,0 +1,13 @@
+package com.volushkova.apifirst.apifirstmockclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstMockClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstMockClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volushkova/apifirst/apifirstmockclient/ApiFirstMockClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volushkova.apifirst.apifirstmockclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstMockClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
